1473f2864c83166846271ee7f0034b98dbcc45b7
manuel murari <manuel.murari@dgsspa.com>
2022-02-22 17:51:11 +0100

    GestioneEmail

---
+++ src/main/java/com/mine/firstIJ/messaging/GmailService.java
@@ -0,0 +1,19 @@
+package com.mine.firstIJ.messaging;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+public class GmailService {
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    public void sendSimpleMessage(String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("noreply@baeldung.com");
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        javaMailSender.send(message);
+    }
+}
